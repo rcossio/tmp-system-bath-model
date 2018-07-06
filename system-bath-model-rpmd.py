e448e597c1cd8a37275261449c54e0245152329e
rodrigo <rodrigoperez93@gmail.com>
2018-07-06 11:52:53 -0300

    la dinamica ya actua de manera razonable. chequeado energia constante. chequeado timestep

--- system-bath-model-rpmd.py
+++ system-bath-model-rpmd.py
@@ -16,11 +16,11 @@ cosh = np.cosh
 #----------------------------------
 #	Parameters
 #----------------------------------
-T    = 1000.0                         # in K
-Kb   = 3.1672083472e-06		      # in Kb 
+T    = 200.0                          # in K
+Kb   = 3.1672083472e-06		      # in Eh/K 
 m    = 1061.0                         # in m_e
 a    = 0.734                          # in a0
-dt   = 8e-2                           # in hbar/Eh
+dt   = 8e0                            # in Eh/hbar
 hbar = 1.0                            # in hbar
 w_c  = 500  *4.5454545454545455e-06   # in Eh
 w_b  = 500  *4.5454545454545455e-06   # in Eh
@@ -28,10 +28,10 @@ V0pp = 2085 *4.5454545454545455e-06   # in Eh
 
 
 nsamples = 1000
-nsteps = 200000
+nsteps = 300000
 Nbids    = 4
 outfile  = sys.argv[1]
-f=3
+f=10
 
 #---------------------------------
 #	Constants
@@ -56,14 +56,15 @@ def c(i):
 
 def calcForce (q):
         F = np.zeros((f,Nbids),dtype=np.float64)
-	F[0,:] = 0.5* m* w_b**2 *q[0,:] - m**2 *w_b**4* q[0,i]**3/(4*V0pp)
+	F[0,:] = m* w_b**2 *q[0,:] - m**2 *w_b**4* q[0,:]**3/(4*V0pp) 
 	for k in range(1,f):
-		F[k,:] = -m* w(k)**2 *q[k,:] +c(k)*q[0,:]
+		F[0,:] += c(k)*(q[k,:]-c(k)*q[0,:]/m/w(k)**2) 
+		F[k,:]  = -m* w(k)**2 *q[k,:] +c(k)*q[0,:]
 
 #	Esto es una mentira: pongo una fuerza armonica para ver como se mueve el polimero
-	for u in range(f):
-		for v in range(Nbids):
-			F[u,v] = -m*w_n**2* q[u,v]/1000
+#	for u in range(f):
+#		for v in range(Nbids):
+#			F[u,v] = - m* w(1)**2 *q[u,v]
 	return F
 
 def calc_derivada_p (q):
@@ -84,7 +85,24 @@ def calcPotential (q):
 		V += -0.5* m* w_b**2 *q[0,i]**2 + m**2 *w_b**4* q[0,i]**4/(16*V0pp)
 		for k in range(1,f):
 			V +=  0.5* m* w(k)**2 *(q[k,i]-c(k)*q[0,i]/(m*w(k)**2))**2
+
+#       Esto es una mentira: pongo una fuerza armonica para ver como se mueve el polimero
+#        for v in range(Nbids):
+#                for u in range(f):
+#                        V +=  0.5* m* w(1)**2 *q[u,v]**2
+
 	return V
+
+def extendedPotential(q):
+	H = calcPotential(q)
+	for k in range(f):
+	        for j in range(Nbids):
+	                if   ( j == 0 ):
+	                        H += 0.5*m*w_n**2*(q[k,j]-q[k,Nbids-1])**2
+	                else:
+	                        H += 0.5*m*w_n**2*(q[k,j]-q[k,j-1])**2
+	return H
+
 
 def deltaV(q):
 	deltav = calcPotential(q)
@@ -96,8 +114,12 @@ def heaviside(q):
 	qq = C.T.dot(q)
         return np.heaviside(np.mean(qq[0,:]), 1.0)
 
-def report(string):
-        output = open(outfile,'a')
+def heaviside_n(q):
+        qq = C.T.dot(q)
+        return np.mean(np.heaviside(qq[0,:], 1.0))
+
+def report(string,filename=outfile):
+        output = open(filename,'a')
         output.write(string)
         output.close()
 
@@ -114,6 +136,8 @@ evals, C = np.linalg.eigh(Hessian)
 
 evals[1:] = evals[1:][::-1]
 C[:,1:] = C[:,1:][:,::-1]
+if C[0,0] < 0.0:
+	C = -C
 w_nm=np.sqrt(evals/m)
 
 #-----------------------------------------
@@ -142,14 +166,15 @@ bf  = []
 weighted = np.zeros(nsamples,dtype=np.float64)
 
 for s in range(nsamples/2):
-	#---------------------------------------
-	#	Center of mass sampling
-	#---------------------------------------
-        #cm = np.zeros(f,dtype=np.float64)
-	#for k in range(1,f):
-	#	sigmacm = 1./np.sqrt(beta_n*m*w_nm[k]**2)
-	#
-	#	cm[k]   = np.random.normal(loc=0.0,scale=sigmacm)
+	#---------------------------------------------------------
+	#	Center of mass sampling in original coordinates
+	#----------------------------------------------------------
+        cm = np.zeros(f,dtype=np.float64)
+#	for k in range(1,f):
+#		sigmacm = 1./np.sqrt(beta_n*m*w_nm[k]**2)
+#		cm[k]   = np.random.normal(loc=0.0,scale=sigmacm)
+#	cmqq = C.T.dot(cm)
+
 	#------------------------------
 	#	Random sampling
 	#------------------------------
@@ -157,7 +182,7 @@ for s in range(nsamples/2):
 	for k in range(f):
 		r = np.random.normal(loc=0.0,scale=1.0,size=Ndim)
 		qq[k,1:Nbids] = CholeskyList[k].dot(r)
-		qq[k,:] += -np.mean(qq[k,:]) -.5 #+cm[k] 
+		qq[k,:] += -np.mean(qq[k,:]) + cm[k] 
 	p = np.random.normal(loc=0.0,scale=sigmap,size=Nbids*f).reshape((f,Nbids))
 
         q = C.dot(qq)
@@ -203,10 +228,20 @@ for s in range(nsamples):
 		p = p + 0.5 * dt * derivada_p
 		t += 1
 
-		if t%100 == 0:
-			report("%10i %14.6g %14.6g %14.6g %14.6g %14.6g %14.6g %14.6g %14.6g %14.6g %14.6g %14.6g %14.6g \n"%(t,q[0,0],q[1,0],q[2,0],q[0,1],q[1,1],q[2,1],q[0,2],q[1,2],q[2,2],q[0,3],q[1,3],q[2,3]))
+		if t%10 == 0:
+			this_cm = np.mean(q,axis=1)
+                        report("%14.6g %14.6g %14.6g %14.6g %14.6g %14.6g %14.6g %14.6g %14.6g %14.6g \n"%tuple(this_cm))
+
+                        this_cm = np.mean(C.T.dot(q),axis=1)
+                        report("%14.6g %14.6g %14.6g %14.6g %14.6g %14.6g %14.6g %14.6g %14.6g %14.6g \n"%tuple(this_cm),filename=sys.argv[2])
+
+			report("%14.6g %14.6g %14.6g\n" %(extendedPotential(q),np.sum(p**2/2/m),extendedPotential(q)+np.sum(p**2/2/m)),filename=sys.argv[3])
+
+
+#			report("%10i %14.6g %14.6g %14.6g %14.6g %14.6g %14.6g %14.6g %14.6g %14.6g %14.6g %14.6g %14.6g \n"%(t,q[0,0],q[1,0],q[2,0],q[0,1],q[1,1],q[2,1],q[0,2],q[1,2],q[2,2],q[0,3],q[1,3],q[2,3]))
+#			report("%14.6g %14.6g\n" %(heaviside_n(q),heaviside(q)))
 		
-	exit() 
+	exit()
 	#---------------------------------------------------------
 	#	Find weight h_n
 	#---------------------------------------------------------
